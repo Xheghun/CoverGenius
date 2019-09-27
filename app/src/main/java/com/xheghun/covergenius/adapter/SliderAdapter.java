18cbf25e758cfbe4c3de9808af179e4ba6c96cb3
Xheghun <ysenruf@gmail.com>
2019-09-27 13:04:06 +0100

    new home page(fix)

--- app/src/main/java/com/xheghun/covergenius/adapter/SliderAdapter.java
+++ app/src/main/java/com/xheghun/covergenius/adapter/SliderAdapter.java
@@ -1,32 +1,53 @@
 package com.xheghun.covergenius.adapter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.xheghun.covergenius.R;
 import com.xheghun.covergenius.model.SliderData;
 
 import java.util.List;
 
-public class SliderAdapter extends RecyclerView.Adapter {
+public class SliderAdapter extends RecyclerView.Adapter<SliderAdapter.SliderViewHolder> {
+    private Context mContext;
+    private List<SliderData> mSliderData;
+
     public SliderAdapter(Context context, List<SliderData> sliderDataList) {
+        mContext = context;
+        mSliderData = sliderDataList;
     }
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public SliderViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.update_item_layout,parent,false);
+        return new SliderViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull SliderViewHolder holder, int position) {
+        Glide.with(mContext).load(R.drawable.profile_img).override(holder.imageView.getWidth(),holder.imageView.getHeight())
+                .into(holder.imageView);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return 6;
+    }
+
+
+    class SliderViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+        SliderViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.post_thumbnail);
+        }
     }
 }
